39607df4ed1270ac457d2e2ed615eba41b8370cd
JMEJIAS <jhonatan.mejias@zentagroup.com>
2022-06-15 23:27:13 -0400

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Material App',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Pay Parking'),
+        ),
+        body: Center(
+          child: Container(
+            child: Text('Hello World'),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/ui/pages/register/register.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class RegisterUser extends StatefulWidget {
+  RegisterUser({Key? key}) : super(key: key);
+
+  @override
+  State<RegisterUser> createState() => _RegisterUserState();
+}
+
+class _RegisterUserState extends State<RegisterUser> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
